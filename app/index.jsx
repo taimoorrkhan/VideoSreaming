29d099f314982517252c0aa75efe41a4adf5351b
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2024-05-05 00:36:04 +0500

    Update index.jsx with new imports and UI changes

--- app/index.jsx
+++ app/index.jsx
@@ -1,21 +1,52 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { Link } from "expo-router";
-
+import { router,Redirect } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
 export default function App() {
 	return (
-		<View>
-			<Text className="text-cyan-300">index</Text>
-			<Link
-				href={"/profile"}
-				style={{
-					color: "blue",
-				}}
-			>
-				Profile
-			</Link>
-		</View>
+		<SafeAreaView className="bg-primary h-full">
+			<ScrollView contentContainerStyle={{ height: "100%" }}>
+				<View className="w-full justify-start  items-center min-h-[85vh] px-4">
+					<Image
+						source={images.logo}
+						resizeMode="contain"
+						className="w-[130px] h-[84px]"
+					/>
+					<Image
+						source={images.cards}
+						resizeMode="contain"
+						className="max-w-[380px] w-full h-[300px]"
+					/>
+					<View className="relative mt-5">
+						<Text className="text-3xl text-white font-bold text-center">
+							Discover Endless Possibilities with {""}
+							<Text className="text-secondary-200">
+								Aora
+							</Text>
+						</Text>
+						<Image
+							source={images.path}
+							className="absolute w-[136px] h-[15px] -bottom-2 -right-8"
+							resizeMode="contain"
+						/>
+					</View>
+					<Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+						Where Creativity Meets Innovation: Embark on a
+						Journey of Limitless Exploration with Aora
+					</Text>
+					<CustomButton
+						containerStyles="w-full mt-5"
+						title="Continue with Email"
+						onPress={() => router.push("/sign-in")}
+					/>
+				</View>
+			</ScrollView>
+			<StatusBar style={"light"} backgroundColor="#161622" />
+		</SafeAreaView>
 	);
 }
 
-const styles = StyleSheet.create({});
+
