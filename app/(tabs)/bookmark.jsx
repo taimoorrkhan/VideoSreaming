8624f5090d34d4729e42d579cee7656af73310de
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2024-07-05 21:29:57 +0500

    created file

--- app/(tabs)/bookmark.jsx
+++ app/(tabs)/bookmark.jsx
@@ -1,10 +1,8 @@
-import { View, Text } from 'react-native'
+import { View, Text,  FlatList } from 'react-native'
 import React from 'react'
-
+import { SafeAreaView } from 'react-native-safe-area-context'
 export default function Bookmark() {
   return (
-    <View>
-      <Text>bookmark</Text>
-    </View>
+  <></>
   )
 }
